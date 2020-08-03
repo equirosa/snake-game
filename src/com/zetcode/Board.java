3cb99aa218b55146081bf639f0cb12d24ff9494e
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-08-03 16:08:08 -0600

    optimise swing imports

--- src/com/zetcode/Board.java
+++ src/com/zetcode/Board.java
@@ -13,9 +13,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import javax.swing.ImageIcon;
-import javax.swing.JPanel;
-import javax.swing.Timer;
+import javax.swing.*;
 
 public class Board extends JPanel implements ActionListener {
 
